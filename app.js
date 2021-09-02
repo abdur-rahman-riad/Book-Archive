f8add5f1838dbf5d4b74ee209d7c6a2b383e5b10
Mohammad Abdur Rahman <rahmanriad.cse@gmail.com>
2021-09-02 20:01:27 +0600

    Dynamic Book Archive Complete

--- app.js
+++ app.js
@@ -1,39 +1,51 @@
+
 const searchFiled = document.getElementById('search-filed');
 const searchButton = document.getElementById('search-button');
-// const searchValue = searchFiled.value;
 
+// Fetching Data When Search Button Clicked.
 searchButton.addEventListener('click', function () {
+    toggleSpinner('block');
     const url = `https://openlibrary.org/search.json?q=${searchFiled.value}`;
-    console.log(url);
     fetch(url)
         .then(response => response.json())
         .then(data => displayBooks(data))
 });
 
+// Display Book and Details
 const displayBooks = bookDetails => {
+    defaultDesign('none');
     // Error Checking 
-    if (bookDetails.q === '') {
-        console.log("Empty Error Message");
-        document.getElementById('empty-message').style.display = 'block';
+    if (searchFiled.value === '') {
+        defaultDesign('none');
+        emptyErrorMessage('block');
+        error404Message('none');
+        toggleSpinner("none");
         searchFiled.value = '';
-    } else {
-        document.getElementById('empty-message').style.display = 'none';
+    } else if (bookDetails.numFound === 0 && bookDetails.q !== '') {
+        defaultDesign('none');
+        error404Message('block');
+        emptyErrorMessage('none');
+        toggleSpinner("none");
+        searchFiled.value = '';
     }
+    else {
+        emptyErrorMessage('none');
+        error404Message('none');
+    }
+
+    // Total Search Result & Clean Container in Every Search
     const bookContainer = document.getElementById('books-container');
     bookContainer.innerHTML = '';
-    const allBooks = bookDetails.docs;
-    // Total Result Found & Show
-    console.log("Result Found: ", allBooks.length);
-    const books = allBooks.slice(0, 50);
-    console.log("Showing: ", books.length);
-    document.getElementById('shown-result').innerText = `Showing ${books.length} Result out of ${allBooks.length}`;
-    // Getting Single Book Info
-    for (const book of books) {
+    const books = bookDetails.docs;
+    document.getElementById('shown-result').innerText = `Search Result: ${bookDetails.numFound} Results`;
+
+    // Getting Single Book Info and Set in Card Design
+    books.forEach(book => {
         const newBook = document.createElement('div');
         newBook.innerHTML = `
         <div class="col h-100">
             <div class="p-2  bg-white text-center h-100 custom-shadow">
-                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="rounded-top" width="100%" height="300px" alt="Book Cover">
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="rounded-top" width="100%" height="300px" alt="${book.title} Cover Photo">
                 <h6 class="m-0 fw-bold">${book.title}</h6>
                 <p class="m-0">${book.author_name[0]}</p>
                 <p class="m-0 fst-italic"> First Published In: ${book.first_publish_year}</p>
@@ -41,14 +53,22 @@ const displayBooks = bookDetails => {
         </div>
         `;
         bookContainer.appendChild(newBook);
-
-        // Showing in Console
-        console.log("Book Name: ", book.title);
-        console.log("Author Name: ", book.author_name[0]);
-        console.log("First Published : ", book.first_publish_year);
-
         searchFiled.value = '';
-    }
+        toggleSpinner('none');
+    });
+}
 
+// Function for Spinner, Default Design and Error Messgae
+const toggleSpinner = spinner => {
+    document.getElementById('spinner-id').style.display = spinner;
+}
+const defaultDesign = homeDesign => {
+    document.getElementById('default-design').style.display = homeDesign;
+}
+const emptyErrorMessage = emptyError => {
+    document.getElementById('empty-message').style.display = emptyError;
+}
+const error404Message = error404 => {
+    document.getElementById('error-404-message').style.display = error404;
 }
 
